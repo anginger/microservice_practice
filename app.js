8c6a82bc6a73e110f401cd3e9aecf35a53fbc6f3
SuperSonic <supersonic@livemail.tw>
2021-10-11 23:16:27 +0800

    Update

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+const statusCode = require('http-status-codes');
 const express = require('express');
 const app = express();
 const port = 3000
 
 app.get('/', function(req, res) {
-    res.json({
-        name: "essential",
-        description: "Star Inc. Web 研發技能訓練教學 ",
-        website: "https://github.com/star-inc/essential",
-        copyright: "(c)2021 Star Inc."
+    res.status(statusCode.OK).send({
+        status: statusCode.OK,
+        data: {
+            description: "Star Inc. Web 研發技能訓練教學 ",
+            information: "https://github.com/star-inc/essential",
+            copyright: "(c)2021 Star Inc."
+        }
     });
 });
 
