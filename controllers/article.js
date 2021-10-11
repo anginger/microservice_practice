e3174a3baf49533caaac50a74b209b60ddd864a1
SuperSonic <supersonic@livemail.tw>
2021-10-11 23:48:26 +0800

    Update

--- controllers/article.js
+++ controllers/article.js
@@ -3,24 +3,37 @@ const express = require('express');
 const router = express.Router();
 
 const {Sequelize} = require("sequelize");
-const sequelize = new Sequelize('sqlite::memory:');
+const memoryHandler = new Sequelize('sqlite::memory:');
 
 const Article = require('../models/article')
 
 router.use(express.urlencoded());
+
+router.get('/list', function (_, response) {
+    Article.findAll().then(articles => {
+        response.status(statusCode.OK).send({
+            status: statusCode.OK,
+            data: articles
+        });
+    });
+})
 
 router.post('/', function (request, response) {
     if (!(request.body.title && request.body.content)) {
         response.status(statusCode.BAD_REQUEST).send({
             status: statusCode.BAD_REQUEST
         });
+    } else {
+        memoryHandler.sync().then(() => {
+            Article.create({
+                title: request.body.title,
+                content: request.body.content,
+            }).then((data) => console.log(data.toJSON()));
+        })
+        response.status(statusCode.CREATED).send({
+            status: statusCode.CREATED
+        });
     }
-    sequelize.sync().then(() => {
-        Article.create({
-            title: request.body.title,
-            content: request.body.content,
-        }).then((data) => console.log(data.toJSON()));
-    })
 });
 
 module.exports = router;
